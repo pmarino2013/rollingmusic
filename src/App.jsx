d57ae4baf4403231edc27a62e9e7affed589c0d7
Juan Narese <juanjo.narese@gmail.com>
2025-04-24 16:40:43 -0300

    app limpio para hacer el merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomeScreen from "./pages/HomeScreen";
-import AdminScreen from "./pages/AdminScreen";
-import SongScreen from "./pages/SongScreen";
-
 
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-       
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/song/:id" element={<SongScreen />} />
-          <Route path="/admin" element={<AdminScreen />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
