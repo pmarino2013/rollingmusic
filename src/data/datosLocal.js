35fda8aaa23d832b62122908924a59827efaba50
Juan Narese <juanjo.narese@gmail.com>
2025-04-25 15:45:27 -0300

    favoritos terminado

--- src/data/datosLocal.js
+++ src/data/datosLocal.js
@@ -6,7 +6,7 @@ export const datosLocal=[
         genero: "Rock",
         pista:"1L94M3KIu7QluZe63g64rv",
         cover:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxMvjP9Js5QGDgrmzuwxl1ngf7gurGgzFppkvcbt0ojauh7vEKbYPZ_ydeVF0zdXWYEqU&usqp=CAU",
-        Favorito: "true"
+       
         
     },
 
@@ -19,7 +19,7 @@ export const datosLocal=[
         genero: "Rock",
         pista:"7ERzS9KIggtCp9oslPzEc0",
         cover:"https://cdn-images.dzcdn.net/images/cover/cd0e3a154d65ca531f51ae6e01aa3371/1900x1900-000000-80-0-0.jpg",
-        Favorito: "true"
+       
     },
 
 
@@ -30,7 +30,7 @@ export const datosLocal=[
         genero: "Reggae",
         pista:"2Zw5FtdFtvKWg4h44GOv3R",
         cover:"https://pelagatos.com.ar/wp-content/uploads/2021/11/bob-marley-and-the-wailers-catch-a-fire.jpg",
-        Favorito: "true"
+        
     },
     
     {
@@ -40,24 +40,24 @@ export const datosLocal=[
         genero: "Rock",
         pista:"7Jh1bpe76CNTCgdgAdBw4Z",
         cover:"https://cdn-images.dzcdn.net/images/cover/5fb91018679f65199308256be3c584ab/0x1900-000000-80-0-0.jpg",
-        Favorito: "true"
+        
     },
     {
         id:5,
-        cancion :"Heroes" ,
-        artista: "David Bowie",
+        cancion :"Dogs" ,
+        artista: "Pink Floyd",
         genero: "Rock",
-        pista:"7Jh1bpe76CNTCgdgAdBw4Z",
-        cover:"https://cdn-images.dzcdn.net/images/cover/5fb91018679f65199308256be3c584ab/0x1900-000000-80-0-0.jpg",
-        Favorito: "true"
+        pista:"2jvuMDqBK04WvCYYz5qjvG",
+        cover:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcqGdV86KzLK4F9QTQ1KZBs2BNbbG_blNuEw&s",
+        
     },
     {
         id:6,
-        cancion :"Heroes" ,
-        artista: "David Bowie",
+        cancion :"Back to Black" ,
+        artista: "Amy Winehouse",
         genero: "Rock",
-        pista:"7Jh1bpe76CNTCgdgAdBw4Z",
-        cover:"https://cdn-images.dzcdn.net/images/cover/5fb91018679f65199308256be3c584ab/0x1900-000000-80-0-0.jpg",
+        pista:"3FAclTFfvUuQYnEsptbK8w",
+        cover:"https://www.infobae.com/resizer/v2/https%3A%2F%2Fs3.amazonaws.com%2Farc-wordpress-client-uploads%2Finfobae-wp%2Fwp-content%2Fuploads%2F2017%2F05%2F04195834%2FRock-Amy-Winehouse.jpg?auth=af663a852ff6e4286b8d4c2769fe46b95466590fe2a6c492298b5991a4d0ae22&smart=true&width=1200&height=900&quality=85",
         Favorito: "true"
     }
 ]
